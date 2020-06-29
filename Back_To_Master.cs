ff137f4267d21d315fadc853e655bb325e18ffa5
AihasasYadu <AihasasYadu@users.noreply.github.com>
2020-06-30 02:25:20 +0530

    To get back to Master Scene

---
+++ Back_To_Master.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Back_To_Master : MonoBehaviour
+{
+    public Button Back;
+    public string Master;
+    public Animator tr;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Back.onClick.AddListener(ButtonClick);
+    }
+
+    void ButtonClick()
+    {
+        SceneManager.LoadScene(Master);
+    }
+}
