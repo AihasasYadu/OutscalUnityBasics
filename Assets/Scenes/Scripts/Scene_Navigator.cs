5a29680c7e5021da762fd310845a8c636b79c9ee
AihasasYadu <AihasasYadu@users.noreply.github.com>
2020-07-01 13:57:04 +0530

    Optimized Scene_Navigator Code

---
+++ Assets/Scenes/Scripts/Scene_Navigator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scene_Navigator : MonoBehaviour
+{
+    public Button B;
+    public string Scene;
+
+    // Update is called once per frame
+    void Awake()
+    {
+        if(B.name.Equals("Exit Button"))
+        {
+            B.onClick.AddListener(delegate { Debug.Log("Quit"); Application.Quit(); });
+        }
+        else
+        {
+            B.onClick.AddListener(ButtonClick);
+        }
+    }
+
+    void ButtonClick()
+    {
+        SceneManager.LoadScene(Scene);
+    }
+}
